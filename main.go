32b83559837f17cb80f7840af6d5b90e1de6fdf6
Can Yucel <can@koding.com>
2014-07-20 19:31:11 -0700

    Change tigertonic with martini

--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"net/url"
 	"time"
 
 	irc "github.com/fluffle/goirc/client"
-	"github.com/rcrowley/go-tigertonic"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+	"github.com/martini-contrib/render"
 )
 
 var (
 	quit     chan bool
-	in       chan string
 	nickname *string
 	c        *irc.Conn
 	connMap  map[string]*irc.Conn
@@ -22,42 +21,23 @@ var (
 func init() {
 	quit = make(chan bool)
 	// set up a goroutine to read commands from stdin
-	in = make(chan string)
 	connMap = make(map[string]*irc.Conn)
 }
 
 func main() {
 
-	// go connect("koding-bot", channels)
+	m := martini.Classic()
+	m.Use(render.Renderer())
+	m.Post("/sendMessage", binding.Json(Message{}), sendMessage)
 
-	mux := tigertonic.NewTrieServeMux()
-	// mux.Handle("POST", "/connect", tigertonic.Timed(tigertonic.Marshaled(connect), "connect", nil))
-	// mux.Handle("POST", "/join", tigertonic.Timed(tigertonic.Marshaled(join), "join", nil))
-	mux.Handle("POST", "/sendMessage", tigertonic.Timed(tigertonic.Marshaled(sendMessage), "sendMessage", nil))
-
-	tigertonic.NewServer(":9000", tigertonic.Logged(mux, nil)).ListenAndServe()
+	m.Run()
 
 }
 
-// func connect(u *url.URL, h http.Header, conn *Connection) (int, http.Header, *Response, error) {
-
-// 	registerHandlers(c)
-
-// 	if err := c.Connect(); err != nil {
-// 		return http.StatusBadRequest, nil, &Response{false, err}, nil
-// 	} else {
-// 		fmt.Printf(c.String())
-// 	}
-
-// 	connMap[conn.Nickname] = c
-
-// 	return http.StatusOK, nil, &Response{true, nil}, nil
-// }
-
 type Message struct {
-	Nickname  string    `json:"nickname"`
-	Body      string    `json:"body"`
-	Channel   string    `json:"channel"`
+	Nickname  string    `json:"nickname" binding:"required"`
+	Body      string    `json:"body" binding:"required"`
+	Channel   string    `json:"channel" binding:"required"`
 	Timestamp time.Time `json:"timestamp"`
 }
 
@@ -67,13 +47,10 @@ type Response struct {
 }
 
 func connect(m *Message) (*irc.Conn, error) {
-	fmt.Println("hu")
 	conn, ok := connMap[m.Nickname]
 	if ok {
 		return conn, nil
 	}
-
-	fmt.Println("ho")
 
 	cfg := irc.NewConfig(m.Nickname)
 	cfg.SSL = true
@@ -87,31 +64,24 @@ func connect(m *Message) (*irc.Conn, error) {
 		fmt.Printf(c.String())
 	}
 
-	// time.After(500 * time.Millisecond)
-
 	connMap[m.Nickname] = c
 
 	return c, nil
 }
 
-func sendMessage(_ *url.URL, _ http.Header, m *Message) (int, http.Header, *Response, error) {
-	fmt.Println("mey")
-	conn, err := connect(m)
+func sendMessage(_ martini.Params, m Message, r render.Render) {
+	conn, err := connect(&m)
 	if err != nil {
-		return http.StatusBadRequest, nil, &Response{Response: false, Error: err}, nil
+		r.JSON(400, Response{Response: false, Error: err})
+		return
 	}
-
-	fmt.Println("hey")
 
 	channel := fmt.Sprintf("#%s", m.Channel)
 	conn.Join(channel)
 
-	// time.Sleep()
-
-	fmt.Println("body", m.Body)
 	conn.Privmsg(channel, m.Body)
 
-	return http.StatusOK, nil, &Response{true, nil}, nil
+	r.JSON(200, Response{true, nil})
 }
 
 func registerHandlers(c *irc.Conn) {
