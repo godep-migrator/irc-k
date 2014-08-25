b7a4ff08458c6a2eb87ea85cf979646253574cce
Can Yucel <can@koding.com>
2014-08-26 00:52:11 +0300

    Client: Add subscriber comments

--- client/subscriber.go
+++ client/subscriber.go
@@ -9,7 +9,10 @@ import (
 	"gopkg.in/redis.v2"
 )
 
+// Subscriber holds the receive channel for fetching
+// messages of subscribed channels
 type Subscriber struct {
+	// message reception channel
 	Rcv chan common.Message
 
 	redisConn *redis.Client
@@ -17,6 +20,8 @@ type Subscriber struct {
 	quit      bool
 }
 
+// NewSubscriber creates redis and pubsub connections and opens
+// receive channel
 func NewSubscriber(r *common.RedisConf) *Subscriber {
 	s := new(Subscriber)
 
@@ -27,18 +32,20 @@ func NewSubscriber(r *common.RedisConf) *Subscriber {
 	return s
 }
 
+// Subscribe used for subscribing a user to given channel messages.
 func (s *Subscriber) Subscribe(channel string) error {
 	if channel == "" {
 		return ErrChannelNotSet
 	}
 
-	// add channel to members channels
+	// subscribe user to given channel for receiving channel messages
 	err := s.ps.Subscribe(common.KeyWithPrefix(channel))
 	if err != nil {
 		return err
 	}
 
-	// add channel to global channels
+	// add subscribed channel to requested channels set. it is used for
+	// preventing duplicate channel connections
 	response := s.redisConn.SAdd(common.KeyWithPrefix(common.REQ_CHANNELS_KEY), channel)
 	if response.Err() != nil {
 		return response.Err()
@@ -49,6 +56,7 @@ func (s *Subscriber) Subscribe(channel string) error {
 		return nil
 	}
 
+	// queue channel name for feeder connection. this queue is consumed by feeder workers.
 	if err := common.MustGetQueue().Queue(channel); err != nil {
 		return err
 	}
@@ -56,16 +64,21 @@ func (s *Subscriber) Subscribe(channel string) error {
 	return nil
 }
 
+// Listen starts listening channel messages in blocking manner.
 func (s *Subscriber) Listen() {
 	for {
 		res, err := s.ps.Receive()
 		if err != nil {
+			// when connection is closed, it returns err.
+			// if quit flag is set, this err message is ignored.
 			if s.quit {
 				return
 			}
 			panic(err)
 		}
 
+		// res also returns subscription events.
+		// we are ignoring them and just listening messages here
 		switch res.(type) {
 		case *redis.Message:
 			rm := res.(*redis.Message)
@@ -81,12 +94,14 @@ func (s *Subscriber) Listen() {
 	}
 }
 
+// Close ends pub/sub and redis connections and sets quit flag to true
 func (s *Subscriber) Close() error {
 	s.quit = true
 	s.ps.Close()
 	return s.redisConn.Close()
 }
 
+// removePrefix is used for removing redis cache key prefix.
 func removePrefix(channel string) string {
 	return strings.Replace(channel, common.PREFIX+":", "", 1)
 }
